93bb5d955f9377a49cee1aacd2df2fe9f2ff01d3
Joshua Simpson <jsimpson@olympic.edu>
2025-01-31 11:44:23 -0800

    Added State and Country tables to verify data. Created SQL script to fill in the data for the above tables.

--- CampusAssetManagementDB/Country.sql
+++ CampusAssetManagementDB/Country.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE [dbo].[Country]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
+﻿CREATE TABLE [dbo].[Country] (
+    [CountryID] INT PRIMARY KEY IDENTITY(1,1), -- Primary Key, Auto-increment by 1 starting at 1
+    [CountryName] NVARCHAR(100) UNIQUE NOT NULL, -- Full country name
+    [CountryCode] NVARCHAR(3) UNIQUE NOT NULL CHECK (LEN(CountryCode) = 3) -- ISO-3166 Alpha-3 Code
+);
--- CampusAssetManagementDB/State.sql
+++ CampusAssetManagementDB/State.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE [dbo].[State]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
+﻿CREATE TABLE [dbo].[State] (
+    [StateID] INT PRIMARY KEY IDENTITY(1,1), -- Primary Key, Auto-increment by 1 starting at 1
+    [StateName] NVARCHAR(100) UNIQUE NOT NULL, -- Full state/province name
+    [StateCode] NVARCHAR(2) UNIQUE NOT NULL CHECK (StateCode LIKE '[A-Z][A-Z]'), -- Two-letter state code
+    [CountryID] INT NOT NULL, -- Foreign key reference
+    FOREIGN KEY ([CountryID]) REFERENCES [dbo].[Country]([CountryID]) ON DELETE CASCADE
+);
