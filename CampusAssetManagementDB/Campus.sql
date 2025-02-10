1e6aea2e11b4a9c6e8dbc7cab9bb823369d68bc7
Joshua Simpson <jsimpson@olympic.edu>
2025-02-10 12:39:30 -0800

    Updated campus. Changed State to StateID with added foreign key constraints to institution and room.

--- CampusAssetManagementDB/Campus.sql
+++ CampusAssetManagementDB/Campus.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[Campus]
 (
-	[CampusId] INT PRIMARY KEY IDENTITY(1,1), -- Primary Key, Auto-increment by 1 starting at 1
+    [CampusId] INT PRIMARY KEY IDENTITY(1,1), -- Primary Key, Auto-increment by 1 starting at 1
     [Name] NVARCHAR(100) NOT NULL, -- Name of the campus (required)
     [Address] NVARCHAR(255) NOT NULL, -- Address of the campus (required)
     [City] NVARCHAR(100) NOT NULL, -- City of the campus (required)
-    [State] NVARCHAR(2) NOT NULL, -- State / Territory of the campus (required)
+    [StateID] INT NOT NULL, -- State / Territory of the campus (required)
     [ZipCode] NVARCHAR(10) NOT NULL, -- Zip code of the campus (required)
     [Country] NVARCHAR(100) NOT NULL DEFAULT 'UNITED STATES', -- Country of the campus (required)
     [HQ] BIT NOT NULL DEFAULT 0, -- Is this the headquarters of the institution? (required)
     [InstitutionId] INT NULL DEFAULT 1, -- Linked to an institution, No foreign key until multi-institution support is added
     [CreatedAt] DATE NULL DEFAULT GETDATE(), -- Date the campus was added to the system
-)
+    CONSTRAINT [FK_Campus_State] FOREIGN KEY ([StateID]) REFERENCES [dbo].[State] ([StateID]),
+    CONSTRAINT [FK_Campus_Institution] FOREIGN KEY ([InstitutionId]) REFERENCES [dbo].[Institution] ([InstitutionId])
+);
--- CampusAssetManagementDB/Room.sql
+++ CampusAssetManagementDB/Room.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Room]
 (
     [RoomID] INT PRIMARY KEY IDENTITY(1,1), -- Primary Key, Auto-increment by 1 starting at 1
-    [RoomNumber] NVARCHAR(50) NOT NULL,  -- Could be "101", "A1", "B203"
     [BuildingID] INT NOT NULL,  -- Linked to a building, see foreign key constraint below
+    [RoomNumber] NVARCHAR(50) NOT NULL,  -- Could be "101", "A1", "B203"
     [RoomTypeID] INT NULL,  -- Semi-Optional (e.g., Office, Lab, Classroom) Triggered by RoomType table
     [Capacity] INT NULL,  -- Optional: For classrooms/labs/meeting rooms
     [CreatedAt] DATETIME DEFAULT GETDATE(), -- Date the room was added to the system
